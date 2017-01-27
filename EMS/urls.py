3d6f6e96fede9599bc2cd08a929a847e567e87aa
natarajkr007 <natarajkr007@gmail.com>
2017-01-27 20:39:40 +0530

    indented

--- EMS/urls.py
+++ EMS/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'EMS'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-url(r'^register/$', views.register, name='register'),
-url(r'^login/$', views.user_login, name='login'),
-url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
