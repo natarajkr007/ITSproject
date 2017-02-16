e5368ba680a5ddecf4178633cd80754ac0a5a7da
nataraj <natarajkr007@gmail.com>
2017-02-16 18:18:28 +0530

    js file of register

---
+++ EMS/static/js/register.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//   $(document).getElementById('loginButton').onclick(function(){
+//     location.href = "www.google.com";
+//   });
+// });
+
+$(document).getElementById("loginButton").onclick = function () {
+        location.href = "www.google.com";
+    };
+//
+// $(document).ready(function() {
+// 	$(window).scroll(function() {
+//   	if($(document).scrollTop() > 10) {
+//     	$('#transition-nav').addClass('shrink');
+//     }
+//     else {
+//     $('#transition-nav').removeClass('shrink');
+//     }
+//   });
+// });
+//
+// $(document).getElementById("loginButton").onclick = function () {
+//   location.href = "{% url "EMS:login" %}";
+// };
